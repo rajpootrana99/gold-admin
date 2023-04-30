1414d58e983ac033fee667ee5b10daa9840f8074
Muhammad Rana <muhammadrana523@gmail.com>
2023-04-30 20:07:14 +0500

    Changes in Gold Index Page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -198,14 +198,14 @@
 
                 <div class="row match-height">
                     <!-- Company Table Card -->
-                    <div class="col-lg-8 col-12">
+                    <div class="col-lg-12 col-12">
                         <div class="card card-company-table">
                             <div class="card-body p-0">
                                 <div class="table-responsive">
                                     <table class="table">
                                         <thead>
                                             <tr>
-                                                <th>Company</th>
+                                                <th>Shop</th>
                                                 <th>Category</th>
                                                 <th>Views</th>
                                                 <th>Revenue</th>
@@ -229,12 +229,12 @@
                                                 </td>
                                                 <td>
                                                     <div class="d-flex align-items-center">
-                                                        <div class="avatar bg-light-primary mr-1">
+                                                        <div class="avatar bg-light-warning mr-1">
                                                             <div class="avatar-content">
-                                                                <i data-feather="monitor" class="font-medium-3"></i>
+                                                                <i data-feather="watch" class="font-medium-3"></i>
                                                             </div>
                                                         </div>
-                                                        <span>Technology</span>
+                                                        <span>Fashion</span>
                                                     </div>
                                                 </td>
                                                 <td class="text-nowrap">
@@ -267,12 +267,12 @@
                                                 </td>
                                                 <td>
                                                     <div class="d-flex align-items-center">
-                                                        <div class="avatar bg-light-success mr-1">
+                                                        <div class="avatar bg-light-warning mr-1">
                                                             <div class="avatar-content">
-                                                                <i data-feather="coffee" class="font-medium-3"></i>
+                                                                <i data-feather="watch" class="font-medium-3"></i>
                                                             </div>
                                                         </div>
-                                                        <span>Grocery</span>
+                                                        <span>Fashion</span>
                                                     </div>
                                                 </td>
                                                 <td class="text-nowrap">
@@ -343,12 +343,12 @@
                                                 </td>
                                                 <td>
                                                     <div class="d-flex align-items-center">
-                                                        <div class="avatar bg-light-primary mr-1">
+                                                        <div class="avatar bg-light-warning mr-1">
                                                             <div class="avatar-content">
-                                                                <i data-feather="monitor" class="font-medium-3"></i>
+                                                                <i data-feather="watch" class="font-medium-3"></i>
                                                             </div>
                                                         </div>
-                                                        <span>Technology</span>
+                                                        <span>Fashion</span>
                                                     </div>
                                                 </td>
                                                 <td class="text-nowrap">
@@ -381,12 +381,12 @@
                                                 </td>
                                                 <td>
                                                     <div class="d-flex align-items-center">
-                                                        <div class="avatar bg-light-success mr-1">
+                                                        <div class="avatar bg-light-warning mr-1">
                                                             <div class="avatar-content">
-                                                                <i data-feather="coffee" class="font-medium-3"></i>
+                                                                <i data-feather="watch" class="font-medium-3"></i>
                                                             </div>
                                                         </div>
-                                                        <span>Grocery</span>
+                                                        <span>Fashion</span>
                                                     </div>
                                                 </td>
                                                 <td class="text-nowrap">
@@ -487,143 +487,8 @@
                     </div>
                     <!--/ Company Table Card -->
 
-                    <!-- Developer Meetup Card -->
-                    <div class="col-lg-4 col-md-6 col-12">
-                        <div class="card card-developer-meetup">
-                            <div class="meetup-img-wrapper rounded-top text-center">
-                                <img src="{{ asset('app-assets/images/illustration/email.svg')}}" alt="Meeting Pic" height="170" />
-                            </div>
-                            <div class="card-body">
-                                <div class="meetup-header d-flex align-items-center">
-                                    <div class="meetup-day">
-                                        <h6 class="mb-0">THU</h6>
-                                        <h3 class="mb-0">24</h3>
-                                    </div>
-                                    <div class="my-auto">
-                                        <h4 class="card-title mb-25">Developer Meetup</h4>
-                                        <p class="card-text mb-0">Meet world popular developers</p>
-                                    </div>
-                                </div>
-                                <div class="media">
-                                    <div class="avatar bg-light-primary rounded mr-1">
-                                        <div class="avatar-content">
-                                            <i data-feather="calendar" class="avatar-icon font-medium-3"></i>
-                                        </div>
-                                    </div>
-                                    <div class="media-body">
-                                        <h6 class="mb-0">Sat, May 25, 2020</h6>
-                                        <small>10:AM to 6:PM</small>
-                                    </div>
-                                </div>
-                                <div class="media mt-2">
-                                    <div class="avatar bg-light-primary rounded mr-1">
-                                        <div class="avatar-content">
-                                            <i data-feather="map-pin" class="avatar-icon font-medium-3"></i>
-                                        </div>
-                                    </div>
-                                    <div class="media-body">
-                                        <h6 class="mb-0">Central Park</h6>
-                                        <small>Manhattan, New york City</small>
-                                    </div>
-                                </div>
-                                <div class="avatar-group">
-                                    <div data-toggle="tooltip" data-popup="tooltip-custom" data-placement="bottom" data-original-title="Billy Hopkins" class="avatar pull-up">
-                                        <img src="{{ asset('app-assets/images/portrait/small/avatar-s-9.jpg')}}" alt="Avatar" width="33" height="33" />
-                                    </div>
-                                    <div data-toggle="tooltip" data-popup="tooltip-custom" data-placement="bottom" data-original-title="Amy Carson" class="avatar pull-up">
-                                        <img src="{{ asset('app-assets/images/portrait/small/avatar-s-6.jpg')}}" alt="Avatar" width="33" height="33" />
-                                    </div>
-                                    <div data-toggle="tooltip" data-popup="tooltip-custom" data-placement="bottom" data-original-title="Brandon Miles" class="avatar pull-up">
-                                        <img src="{{ asset('app-assets/images/portrait/small/avatar-s-8.jpg')}}" alt="Avatar" width="33" height="33" />
-                                    </div>
-                                    <div data-toggle="tooltip" data-popup="tooltip-custom" data-placement="bottom" data-original-title="Daisy Weber" class="avatar pull-up">
-                                        <img src="{{ asset('app-assets/images/portrait/small/avatar-s-20.jpg')}}" alt="Avatar" width="33" height="33" />
-                                    </div>
-                                    <div data-toggle="tooltip" data-popup="tooltip-custom" data-placement="bottom" data-original-title="Jenny Looper" class="avatar pull-up">
-                                        <img src="{{ asset('app-assets/images/portrait/small/avatar-s-20.jpg')}}" alt="Avatar" width="33" height="33" />
-                                    </div>
-                                    <h6 class="align-self-center cursor-pointer ml-50 mb-0">+42</h6>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!--/ Developer Meetup Card -->
-
-                    <!-- Browser States Card -->
-                    <div class="col-lg-4 col-md-6 col-12">
-                        <div class="card card-browser-states">
-                            <div class="card-header">
-                                <div>
-                                    <h4 class="card-title">Browser States</h4>
-                                    <p class="card-text font-small-2">Counter August 2020</p>
-                                </div>
-                                <div class="dropdown chart-dropdown">
-                                    <i data-feather="more-vertical" class="font-medium-3 cursor-pointer" data-toggle="dropdown"></i>
-                                    <div class="dropdown-menu dropdown-menu-right">
-                                        <a class="dropdown-item" href="javascript:void(0);">Last 28 Days</a>
-                                        <a class="dropdown-item" href="javascript:void(0);">Last Month</a>
-                                        <a class="dropdown-item" href="javascript:void(0);">Last Year</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="card-body">
-                                <div class="browser-states">
-                                    <div class="media">
-                                        <img src="{{ asset('app-assets/images/icons/google-chrome.png')}}" class="rounded mr-1" height="30" alt="Google Chrome" />
-                                        <h6 class="align-self-center mb-0">Google Chrome</h6>
-                                    </div>
-                                    <div class="d-flex align-items-center">
-                                        <div class="font-weight-bold text-body-heading mr-1">54.4%</div>
-                                        <div id="browser-state-chart-primary"></div>
-                                    </div>
-                                </div>
-                                <div class="browser-states">
-                                    <div class="media">
-                                        <img src="{{ asset('app-assets/images/icons/mozila-firefox.png')}}" class="rounded mr-1" height="30" alt="Mozila Firefox" />
-                                        <h6 class="align-self-center mb-0">Mozila Firefox</h6>
-                                    </div>
-                                    <div class="d-flex align-items-center">
-                                        <div class="font-weight-bold text-body-heading mr-1">6.1%</div>
-                                        <div id="browser-state-chart-warning"></div>
-                                    </div>
-                                </div>
-                                <div class="browser-states">
-                                    <div class="media">
-                                        <img src="{{ asset('app-assets/images/icons/apple-safari.png')}}" class="rounded mr-1" height="30" alt="Apple Safari" />
-                                        <h6 class="align-self-center mb-0">Apple Safari</h6>
-                                    </div>
-                                    <div class="d-flex align-items-center">
-                                        <div class="font-weight-bold text-body-heading mr-1">14.6%</div>
-                                        <div id="browser-state-chart-secondary"></div>
-                                    </div>
-                                </div>
-                                <div class="browser-states">
-                                    <div class="media">
-                                        <img src="{{ asset('app-assets/images/icons/internet-explorer.png')}}" class="rounded mr-1" height="30" alt="Internet Explorer" />
-                                        <h6 class="align-self-center mb-0">Internet Explorer</h6>
-                                    </div>
-                                    <div class="d-flex align-items-center">
-                                        <div class="font-weight-bold text-body-heading mr-1">4.2%</div>
-                                        <div id="browser-state-chart-info"></div>
-                                    </div>
-                                </div>
-                                <div class="browser-states">
-                                    <div class="media">
-                                        <img src="{{ asset('app-assets/images/icons/opera.png')}}" class="rounded mr-1" height="30" alt="Opera Mini" />
-                                        <h6 class="align-self-center mb-0">Opera Mini</h6>
-                                    </div>
-                                    <div class="d-flex align-items-center">
-                                        <div class="font-weight-bold text-body-heading mr-1">8.4%</div>
-                                        <div id="browser-state-chart-danger"></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!--/ Browser States Card -->
-
                     <!-- Goal Overview Card -->
-                    <div class="col-lg-4 col-md-6 col-12">
+                    <div class="col-lg-6 col-md-6 col-12">
                         <div class="card">
                             <div class="card-header d-flex justify-content-between align-items-center">
                                 <h4 class="card-title">Goal Overview</h4>
@@ -647,7 +512,7 @@
                     <!--/ Goal Overview Card -->
 
                     <!-- Transaction Card -->
-                    <div class="col-lg-4 col-md-6 col-12">
+                    <div class="col-lg-6 col-md-6 col-12">
                         <div class="card card-transaction">
                             <div class="card-header">
                                 <h4 class="card-title">Transactions</h4>
